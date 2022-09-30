9e1900caba32c482ceb7a9fc8ce93473c255f91f
Conor Bergman <conorbergman@gmail.com>
2022-09-30 14:37:56 -0400

    v2.2.2 Update shared paths in tests

--- test/hris/test_account_details_and_actions.py
+++ test/hris/test_account_details_and_actions.py
@@ -16,7 +16,7 @@ from unittest.mock import MagicMock
 import MergePythonSDK.hris
 from MergePythonSDK.hris.model.account_details_and_actions_integration import AccountDetailsAndActionsIntegration
 from MergePythonSDK.hris.model.account_details_and_actions_status_enum import AccountDetailsAndActionsStatusEnum
-from MergePythonSDK.hris.model.category_enum import CategoryEnum
+from MergePythonSDK.shared.model.category_enum import CategoryEnum
 globals()['AccountDetailsAndActionsIntegration'] = AccountDetailsAndActionsIntegration
 globals()['AccountDetailsAndActionsStatusEnum'] = AccountDetailsAndActionsStatusEnum
 globals()['CategoryEnum'] = CategoryEnum
