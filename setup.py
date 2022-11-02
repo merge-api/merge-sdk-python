2d2957a149baeb645ed053b38bac3f8df2c29146
Conor Bergman <conorbergman@gmail.com>
2022-11-02 12:19:57 -0400

    Bump setup version and update CHANGELOG

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "MergePythonSDK"
-VERSION = "2.2.3"
+VERSION = "2.2.4"
 # To install the library, run the following
 #
 # python setup.py install
